3814752622f6295895f12fb21afba2778911a914
iribel97 <irinaochoa32@gmail.com>
2024-05-07 23:42:52 -0500

    Actividad: Definiendo una plantilla. Finalizada

---
+++ Profile/scripts/profile.js
@@ -0,0 +1,9 @@
+//Defirnir las 5 variables con datos personales
+const nombre = "Irina";
+const apellido = "Ochoa";
+const edad = 23;
+const tipoDeSangre = "B+";
+const carrera = "Ingeniería en Tecnologías de la Información";
+
+//Template string
+document.write(`Hola, mi nombre es ${nombre} ${apellido}, tengo ${edad} años, mi tipo de sangre es ${tipoDeSangre} y estudio la carrera de ${carrera}.`);
