8a723268dcec5b1bcc2228a4bd342efc3798b787
iribel97 <irinaochoa32@gmail.com>
2024-05-08 00:03:50 -0500

    Actividad: Implementando la Asignación con AND lógico (&&). Finalizado

--- Profile/scripts/profile.js
+++ Profile/scripts/profile.js
@@ -4,6 +4,24 @@ const apellido = "Ochoa";
 const edad = 23;
 const tipoDeSangre = "B+";
 const carrera = "Ingeniería en Tecnologías de la Información";
+let isOnline;
 
-//Template string
-document.write(`Hola, mi nombre es ${nombre} ${apellido}, tengo ${edad} años, mi tipo de sangre es ${tipoDeSangre} y estudio la carrera de ${carrera}.`);
+//Se va a repetir mientras el usuario no ingrese "si" o "no"
+do {
+    //Preguntar al usuario si está en línea
+    isOnline = prompt("¿Estás en línea? (SI/NO)").toLowerCase();
+
+    //Validar la respuesta con el operador ternario
+    /**
+     * Si lo que ingresó el usuario no es "si" ni "no", entonces mostrar un mensaje de alerta
+     */
+    (isOnline !== "si" && isOnline !== "no") ? alert("Por favor, ingresa una respuesta válida") : null;
+
+} while (isOnline !== "si" && isOnline !== "no");
+
+//Si el usuario está en línea, entonces mostrara el template string
+if (isOnline === "si") {
+    //Template string
+    document.write(`Hola, mi nombre es ${nombre} ${apellido}, tengo ${edad} años, mi tipo de sangre es ${tipoDeSangre} y estudio la carrera de ${carrera}.`);
+}
+
