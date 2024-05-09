0b10641d0c6c80a23f0a11e0ecd811764a505a75
iribel97 <irinaochoa32@gmail.com>
2024-05-09 10:53:35 -0500

    Actividad: Practicando con la estructuración de objetos. Finalizada

---
+++ Profile/scripts/profile1.js
@@ -0,0 +1,39 @@
+//Datos personales
+const nombre = "Irina";
+const apellido = "Ochoa";
+const edad = 23;
+const fechaNacimiento = "30/11/2000";
+const carrera = "Ingeniería en Tecnologías de la Información";
+
+//Definiendo un objeto usando property shorthand
+const datosPersonales = {
+    nombre,
+    apellido,
+    edad,
+    fechaNacimiento,
+    carrera
+};
+let respuesta;
+do{
+    respuesta = prompt("Esta en linea? SI/NO").toUpperCase();
+    (respuesta !== "SI" && respuesta !== "NO") && alert("Respuesta inválida");
+
+}while(respuesta !== "SI" && respuesta !== "NO");
+
+if(respuesta === "SI"){
+    printProfile();
+}
+
+function printProfile(id = "etiqueta3") {
+    const container = document.getElementById(id);
+
+    // Se crea el template
+    const template = `
+    Hola, mi nombre es ${datosPersonales.nombre} ${datosPersonales.apellido},
+    tengo ${datosPersonales.edad} años, nací el ${datosPersonales.fechaNacimiento} y estudio la carrera de ${datosPersonales.carrera}.
+    `;
+
+    // Se imprime el template en el contenedor
+    container.innerHTML = template;
+}
+
