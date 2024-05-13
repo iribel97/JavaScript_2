efded0b969e401401e5f02c530aa554e5f8e117b
iribel97 <irinaochoa32@gmail.com>
2024-05-13 13:55:10 -0500

    Actividad: Modularización de los datos. FINALIZADA

--- Geometric/scripts/geometric.js
+++ Geometric/scripts/geometric.js
@@ -1,28 +1,25 @@
 import {perimetro} from "./perimeter.js";
 import {area} from "./area.js";
+import { rectangles} from "./rectangleData.js";
 
-//Definiendo 3 objetos
-const rectangulo1 = {
-    altura: 10,
-    ancho: 20
-}
-const rectangulo2 = {
-    altura: 30,
-    ancho: 40
-}
-const rectangulo3 = {
-    altura: 50,
-    ancho: 60
+
+
+
+function imprimirRectangulo(rectangles){
+    const contenido = document.querySelector("main");
+
+    rectangles.forEach((rectangulo, i) => {
+        const div = document.createElement("div");
+        div.innerHTML = `
+            <h1>Rectangulo ${i+1}</h1>
+            <p>Altura: ${rectangulo.altura}</p>
+            <p>Ancho: ${rectangulo.ancho}</p>
+            <p>Area: ${area(rectangulo)}</p>
+            <p>Perimetro: ${perimetro(rectangulo)}</p>
+        `;
+        contenido.appendChild(div);
+
+    });
 }
 
-document.querySelector("main").innerHTML = `
-    <h1>Rectángulos</h1>
-    <p>El perímetro del rectángulo 1 es ${perimetro(rectangulo1)}</p>
-    <p>El área del rectángulo 1 es ${area(rectangulo1)}</p>
-    <p>---------------------------------------------------------------</p>
-    <p>El perímetro del rectángulo 2 es ${perimetro(rectangulo2)}</p>
-    <p>El área del rectángulo 2 es ${area(rectangulo2)}</p>
-    <p>---------------------------------------------------------------</p>
-    <p>El perímetro del rectángulo 3 es ${perimetro(rectangulo3)}</p>
-    <p>El área del rectángulo 3 es ${area(rectangulo3)}</p>
-`;
+imprimirRectangulo(rectangles);
