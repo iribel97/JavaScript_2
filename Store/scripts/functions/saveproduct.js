820184f98d2a3b042602fde219350288c0d09ba6
iribel97 <irinaochoa32@gmail.com>
2024-05-17 01:58:36 -0500

    Actividad: Alerta al Agregar un Producto al Carrito. FINALIZADA

--- Store/scripts/functions/saveproduct.js
+++ Store/scripts/functions/saveproduct.js
@@ -1,31 +1,58 @@
 import { getProducts } from "../products.js";
 
-function saveProd(id) {
-  getProducts().then((products) => {
 
-    const found = products.find((each) => each.id === id);
-    const product = {
-      id: id,
-      title: found.title,
-      price: found.price,
-      description: found.description,
-      image: found.images[0],
-      color: document.querySelector("#color-" + id).value,
-      quantity: Number(document.querySelector("#quantity-" + id).value),
-    };
-    const storage = localStorage.getItem("cart");
-    if (storage) {
-      const parseStorage = JSON.parse(storage);
-      parseStorage.push(product);
-      const stringifyProducts = JSON.stringify(parseStorage);
-      localStorage.setItem("cart", stringifyProducts);
-    } else {
-      const stringifyProduct = JSON.stringify([product]);
-      localStorage.setItem("cart", stringifyProduct);
-    }
-  }).catch((error) => {
-    console.error('Hubo un problema al obtener los productos:', error);
-  })
+function saveProd(id) {
+  console.log("ID del producto:", id); // Verificar el ID recibido
+
+  getProducts()
+    .then((products) => {
+      const found = products.find(product => product.id === id);
+
+      if (!found) {
+        throw new Error("Producto no encontrado");
+      }
+
+      const product = {
+        id: id,
+        title: found.title,
+        price: found.price,
+        description: found.description,
+        image: found.images && found.images.length > 0 ? found.images[0] : "",
+        color: document.querySelector("#color-" + id).value,
+        quantity: Number(document.querySelector("#quantity-" + id).value) || 1,
+      };
+
+      const storage = localStorage.getItem("cart");
+      let cart;
+      if (storage) {
+        cart = JSON.parse(storage);
+      } else {
+        cart = [];
+      }
+
+      if (cart.find((each) => each.id === id && each.color === product.color)) {
+        Swal.fire({
+          title: "Producto añadido",
+          text: "El producto ha sido añadido al carrito",
+          icon: "warning",
+          confirmButtonText: "Aceptar"
+        });
+      } else {
+        cart.push(product);
+        localStorage.setItem("cart", JSON.stringify(cart));
+        Swal.fire({
+          title: "Producto añadido",
+          text: "El producto ha sido añadido al carrito",
+          icon: "success",
+          confirmButtonText: "Aceptar"
+        });
+      }
+
+     
+    })
+    .catch((error) => {
+      console.error('Hubo un problema al obtener los productos:', error);
+    });
 }
 
 export { saveProd };
